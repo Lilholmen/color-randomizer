8ef9f28e29d1f66e5b7f526afb1c0d832ab39710
lilhomen <lilhomen@yandex.ru>
2021-09-27 02:53:06 +0300

    RGB color chooser; color code at the bottom

--- app.js
+++ app.js
@@ -1,33 +1,90 @@
 const background = document.querySelector('#container');
-const ranges = document.querySelectorAll('.input__range');
-const labels = document.querySelectorAll('.settings__label');
-const gen = document.querySelector('.generate-area');
+const gen = document.querySelector('.generate-area__color');
+//const result = document.querySelector('#res');
 
-const settings = {
+const rangesHSL = document.querySelectorAll('.input__range--hsl');
+const rangesRGB = document.querySelectorAll('.input__range--rgb');
+const labelsHSL = document.querySelectorAll('.settings__label--hsl');
+const labelsRGB = document.querySelectorAll('.settings__label--rgb');
+
+const settingsHSL = {
   hue: document.querySelector('#hue').value,
   saturation: document.querySelector('#saturation').value,
   lightness: document.querySelector('#lightness').value,
 };
 
-labels.forEach((label, index) => {
-  label.textContent = ranges[index].value;
+const settingsRGB = {
+  red: document.querySelector('#red').value,
+  green: document.querySelector('#green').value,
+  blue: document.querySelector('#blue').value,
+};
+
+labelsHSL.forEach((label, index) => {
+  label.textContent = rangesHSL[index].value;
 });
 
-ranges.forEach((range, index) => {
+labelsRGB.forEach((label, index) => {
+  label.textContent = rangesRGB[index].value;
+});
+
+rangesHSL.forEach((range, index) => {
   range.addEventListener('input', () => {
-    labels[index].textContent = range.value;
-    background.style.backgroundColor = temp();
+    labelsHSL[index].textContent = range.value;
+    background.style.backgroundColor = generateColorHSL();
+    gen.textContent = calculateResultColor();
+  });
+});
+
+rangesRGB.forEach((range, index) => {
+  range.addEventListener('input', () => {
+    labelsRGB[index].textContent = range.value;
+    background.style.backgroundColor = generateColorRGB();
+    gen.textContent = calculateResultColor();
+    fontColorChanger(
+      +rangesRGB[0].value + +rangesRGB[1].value + +rangesRGB[2].value
+    );
   });
 });
 
 gen.addEventListener('click', () => {
-  background.style.backgroundColor = temp();
+  background.style.backgroundColor = generateColorRGB();
 });
 
-function temp() {
-  return `hsl(${ranges[0].value}, ${ranges[1].value}%, ${ranges[2].value}%)`;
+function generateColorHSL() {
+  return `hsl(${rangesHSL[0].value}, ${rangesHSL[1].value}%, ${rangesHSL[2].value}%)`;
+}
+
+function generateColorRGB() {
+  return `rgb(${rangesRGB[0].value}, ${rangesRGB[1].value}, ${rangesRGB[2].value})`;
 }
 
 function changeBackGroundColor() {
-  background.style.backgroundColor = `hsl{${settings.hue}, ${settings.saturation}%, ${settings.lightness}%}`;
+  background.style.backgroundColor = `hsl{${settingsHSL.hue}, ${settingsHSL.saturation}%, ${settingsHSL.lightness}%}`;
+}
+
+function calculateResultColor() {
+  let color = background.style.backgroundColor;
+  color = color
+    .slice(4, -1)
+    .split(', ')
+    .map((el) => complementColorCode(el))
+    .join('');
+
+  return '#' + color;
+}
+
+function complementColorCode(code) {
+  let temp = Number(code).toString(16);
+
+  return temp.length === 2 ? temp : '0' + temp;
+}
+
+function fontColorChanger(colorSum) {
+  const diff = 765 - colorSum;
+
+  if (diff < 205) {
+    gen.style.color = `rgb(${50 + diff},${50 + diff},${50 + diff})`;
+  } else {
+    gen.style.color = '#fff';
+  }
 }
